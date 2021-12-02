8f9130fcdb7d2e97d6ab2ac0fd9b0ef8c8c35516
Luca Magini <lucamagini7@gmail.com>
2021-12-02 15:07:23 +0100

    ML API Server Created

--- ML-online_learning_server.py
+++ ML-online_learning_server.py
@@ -1,6 +1,7 @@
 import flask
 from flask_selfdoc import Autodoc
 from flask import request
+from Models import Model
 
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
@@ -9,6 +10,7 @@ auto = Autodoc(app)
 
 ip_address = '0.0.0.0'
 
+ML_model = Model()
 
 @app.route('/', methods = ['GET'])
 @auto.doc()
@@ -17,14 +19,14 @@ def home():
     
     try:
         result = {
-                    'Result':"OK",
-                    'Data':"Server Ready"
+                    'Result':'OK',
+                    'Data':'Server Ready'
                  }
             
     except:
         result = {
-                    'Result':"NOT OK",
-                    'Data':"Server Not Ready"
+                    'Result':'NOT OK',
+                    'Data':'Server Not Ready'
                  }
             
     response = flask.jsonify(result)
@@ -32,13 +34,14 @@ def home():
     
     return response  
 
-@app.route('/api/v1/create_model')
+@app.route('/api/v1/create_model', methods = ['GET'])
 @auto.doc(args=['model', 'name', 'intercept_lr', 'loss', 'n_models', 
                 'split_criterion', 'n_neighbors', 'p'])
 def create_model():
     "Create a ML Model for Online Learning"
     
-    models = ['logisticregression', 'randomforest', 'knn', 'gaussiannb']
+    models = ML_model.list
+    
     lr_params = ['model', 'name', 'intercept_lr', 'loss']
     rf_params = ['model', 'name', 'n_models', 'split_criterion']
     knn_params = ['model', 'name', 'n_neighbors', 'p']
@@ -54,7 +57,7 @@ def create_model():
         
         result = {
                     'Result':'NOT OK',
-                    'Data':"Please Insert <model> and <name> Paramaters"
+                    'Data':'Please Insert <model> and <name> Paramaters'
                  }
         
         response = flask.jsonify(result)
@@ -90,15 +93,84 @@ def create_model():
             return response
     
     #Create the model if all checks passed
-    result = {
-                'Result':'OK',
-                'Data':'{} model created'.format(model)
-             }     
+    model = request.args.get('model')
+    name = request.args.get('name')
+    intercept_lr = request.args.get('intercept_lr') if request.args.get('intercept_lr') != None else 0.01    
+    loss = request.args.get('loss') if request.args.get('loss') != None else 'Log'
+    n_models = request.args.get('n_models') if request.args.get('n_models') != None else 10
+    split_criterion = request.args.get('split_criterion') if request.args.get('split_criterion') != None else 'info_gain'
+    n_neighbors = request.args.get('n_neighbors') if request.args.get('n_neighbors') != None else 5
+    p = request.args.get('p') if request.args.get('p') != None else 2
     
-    response = flask.jsonify(result)
-    response.headers.set('Content-Type', 'application/json')
+    try:
+        
+        res, _model = ML_model.create_model(model, name, intercept_lr, loss,
+                                    n_models, split_criterion,
+                                    n_neighbors, p)
+        
+        if res == True:
             
-    return response     
+            if model.lower() == 'logisticregression':
+                
+                if request.args.get('intercept_lr') != None:
+                    _intercept = _model['OneVsRestClassifier'].classifier.intercept_lr
+                else:
+                    _intercept = str(_model['OneVsRestClassifier'].classifier.intercept_lr.learning_rate)
+                
+                data = {'Model': model,
+                        'Name': name,
+                        'Parameters': { 'intercept_lr': _intercept,
+                                        'loss': str(_model['OneVsRestClassifier'].classifier.loss)
+                            }
+                        }
+                
+            elif model.lower() == 'randomforest':
+                
+                data = {'Model': model,
+                        'Name': name,
+                        'Parameters': { 'n_models': _model['AdaptiveRandomForestClassifier'].n_models,
+                                        'split_criterion': _model['AdaptiveRandomForestClassifier'].split_criterion
+                            }
+                        }
+                
+            elif model.lower() == 'gaussiannb':
+                
+                data = {'Model': model,
+                        'Name': name,
+                        }  
+                
+            elif model.lower() == 'knn':
+                
+                data = {'Model': model,
+                        'Name': name,
+                        'Parameters': { 'n_neighbors': _model['KNNClassifier'].n_neighbors,
+                                        'p': _model['KNNClassifier'].p
+                            }
+                        }     
+            
+                
+            result = {
+                        'Result':'OK',
+                        'Data': data
+                     }  
+            
+            response = flask.jsonify(result)
+            response.headers.set('Content-Type', 'application/json')
+            
+            return response
+        
+    except Exception as e:
+        
+        print(e)
+        result = {
+                        'Result':'NOT OK',
+                        'Data':'Model not created'
+                  }  
+            
+        response = flask.jsonify(result)
+        response.headers.set('Content-Type', 'application/json')
+        
+        return response    
 
 
 
--- Models.py
+++ Models.py
@@ -9,80 +9,111 @@ from river.neighbors import KNNClassifier
 
 class Model:
     
-    def __init__(self, model, name):
+    def __init__(self):
         
         #GENERAL PARAMETERS
-        self.model = model
-        self.name = name 
+        self.model = None
+        self.name = None
+        self.list = ['logisticregression', 'randomforest', 
+                       'knn', 'gaussiannb']
         #LOGISTIC REGRESSION PARAMETERS                      
-        self.intercept_lr = 0.01                  
-        self.loss = optim.losses.Log()  
+        self.intercept_lr = None                  
+        self.loss = None  
         #RANDOM FOREST PARAMETERS                    
-        self.n_models = 10                   
-        self.split_criterion = 'info_gain' 
+        self.n_models = None                   
+        self.split_criterion = None 
         #KNN PARAMETERS                 
-        self.n_neighbors = 5
-        self.p = 2
+        self.n_neighbors = None
+        self.p = None
         
-    def create_model(self):
+    def create_model( self, model, name, intercept_lr, 
+                      loss, n_models,
+                      split_criterion, n_neighbors,
+                      p ):
         
-        try:
-            
-            if self.model.lower() == 'logisticregression':
-                #Constructing our pipeline (standardize features + model)
-                model = (
-                    StandardScaler() | 
-                    OneVsRestClassifier(classifier=LogisticRegression(intercept_lr = self.intercept_lr, loss=self.loss))
-                )
-            
-                with open('LogisticRegression_{}.pkl'.format(self.name), 'wb') as f:
-                    pickle.dump(model, f)
-                    
-                return True    
-                    
-            elif self.model.lower() == 'randomforest':
-                #Constructing our pipeline (standardize features + model)
-                model = (
-                    StandardScaler() | 
-                    AdaptiveRandomForestClassifier(n_models = self.n_models, split_criterion = self.split_criterion, seed = 42)
-                )
-            
-                with open('RandomForest_{}.pkl'.format(self.name), 'wb') as f:
-                    pickle.dump(model, f)  
-                    
-                return True    
-                    
-            elif self.model.lower() == 'gaussiannb':
-                #Constructing our pipeline (standardize features + model)
-                model = (
-                    StandardScaler() | 
-                    GaussianNB()
-                )
-            
-                with open('GaussianNB_{}.pkl'.format(self.name), 'wb') as f:
-                    pickle.dump(model, f)
-                    
-                return True    
-                    
-            elif self.model.lower() == 'knn':
-                #Constructing our pipeline (standardize features + model)
-                model = (
-                    StandardScaler() | 
-                    KNNClassifier(n_neighbors = self.n_neighbors, p = self.p)
-                )
-            
-                with open('KNN_{}.pkl'.format(self.name), 'wb') as f:
-                    pickle.dump(model, f)
-                    
-                return True    
-                    
-            else:
-                return False
+        self.model = model
+        self.name = name
+        self.intercept_lr = intercept_lr
+        self.n_models = n_models
+        self.split_criterion = split_criterion
+        self.n_neighbors = n_neighbors
+        self.p = p
+        if loss == 'Hinge':
+            self.loss = optim.losses.Hinge()
+        else:
+            self.loss = optim.losses.Log()
         
-        except Exception as e:
+        classes = [ OneVsRestClassifier(classifier=LogisticRegression(intercept_lr = self.intercept_lr, loss=self.loss)), 
+                    AdaptiveRandomForestClassifier(n_models = self.n_models, split_criterion = self.split_criterion, seed = 42), 
+                    GaussianNB(),
+                    KNNClassifier(n_neighbors = self.n_neighbors, p = self.p) ]
+        
+        mapping = { k:v for k,v in zip(self.list, classes) }
+        
+        if self.model.lower() in mapping:
+            #Constructing our pipeline (standardize features + model)
+            _model = (
+                StandardScaler() | 
+                mapping[self.model.lower()]
+            )
+        
+            with open('{}_{}.pkl'.format(self.model, self.name), 'wb') as f:
+                pickle.dump(_model, f)
+                
+            return True, _model
+        
+        else:
             
-            print(e)
             return False
+            
+        # if self.model.lower() == 'logisticregression':
+        #     #Constructing our pipeline (standardize features + model)
+        #     model = (
+        #         StandardScaler() | 
+        #         OneVsRestClassifier(classifier=LogisticRegression(intercept_lr = self.intercept_lr, loss=self.loss))
+        #     )
+        
+        #     with open('LogisticRegression_{}.pkl'.format(self.name), 'wb') as f:
+        #         pickle.dump(model, f)
+                
+        #     return True    
+                
+        # elif self.model.lower() == 'randomforest':
+        #     #Constructing our pipeline (standardize features + model)
+        #     model = (
+        #         StandardScaler() | 
+        #         AdaptiveRandomForestClassifier(n_models = self.n_models, split_criterion = self.split_criterion, seed = 42)
+        #     )
+        
+        #     with open('RandomForest_{}.pkl'.format(self.name), 'wb') as f:
+        #         pickle.dump(model, f)  
+                
+        #     return True    
+                
+        # elif self.model.lower() == 'gaussiannb':
+        #     #Constructing our pipeline (standardize features + model)
+        #     model = (
+        #         StandardScaler() | 
+        #         GaussianNB()
+        #     )
+        
+        #     with open('GaussianNB_{}.pkl'.format(self.name), 'wb') as f:
+        #         pickle.dump(model, f)
+                
+        #     return True    
+                
+        # elif self.model.lower() == 'knn':
+        #     #Constructing our pipeline (standardize features + model)
+        #     model = (
+        #         StandardScaler() | 
+        #         KNNClassifier(n_neighbors = self.n_neighbors, p = self.p)
+        #     )
+        
+        #     with open('KNN_{}.pkl'.format(self.name), 'wb') as f:
+        #         pickle.dump(model, f)
+                
+        #     return True    
+        
             
             
             
